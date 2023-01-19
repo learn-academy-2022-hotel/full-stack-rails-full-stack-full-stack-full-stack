5009f85090a1d0cae8c3f267baa083045cbdc4cb
djx-ctrl <cras.dtravis@gmail.com>
2023-01-19 11:39:42 -0800

    finished create criteria

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get 'blogs' => 'blog#index', as: 'blogs'
   get 'blogs/new' => 'blog#new', as: 'add_blog'
   get 'blogs/:id' => 'blog#show', as: 'blog'
+  post 'blogs' => 'blog#create'
 
   # root "articles#index"
 end
